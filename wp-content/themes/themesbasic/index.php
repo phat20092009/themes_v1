aaceb43ea82cdd61e920301b9e486289cba97324
phatnguyen <phatnguyenit1991@gmail.com>
2015-07-26 23:38:41 +0700

    home change

--- wp-content/themes/themesbasic/index.php
+++ wp-content/themes/themesbasic/index.php
@@ -3,10 +3,9 @@
 <div class="content">
  
 	<section id="main-content">
-	<?php ?>
 	        <?php if ( have_posts() ) : 
 	        		while ( have_posts() ) : the_post(); ?>
-	                <?php get_template_part( 'content', get_post_format() ); ?>
+	               	 <?php get_template_part( 'content', get_post_format() ); ?>
 	        <?php 	endwhile; ?>
 	        <?php thachpham_pagination(); ?>
 	        <?php else : ?>
